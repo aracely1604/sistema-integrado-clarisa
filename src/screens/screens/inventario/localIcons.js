9c64b9da2fff3bc0f0f95355d23d4e48ade9414f
Tito Henrry Urrelo Aquino <tito.urrelo@inacapmail.cl>
2026-09-08 18:02:19 -0300

    duplicate removal

--- src/screens/screens/inventario/localIcons.js
+++
@@ -1,9 +0,0 @@
-// screens/inventario/localIcons.js
-// Mapeo de los íconos de MaterialCommunityIcons del original a react-icons/md.
-import { MdFastfood, MdInventory2, MdLocalCafe } from 'react-icons/md';
-
-export const LOCAL_ICON_COMPONENTS = {
-  comidaRapida:    MdFastfood,
-  almacen:   MdInventory2,
-  cafeteria: MdLocalCafe,
-};
